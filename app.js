3261d71a1a36ca60cce1003d0483b2315279d5af
Zoro444 <zoro.simonyan1995@gmail.com>
2023-06-01 20:50:55 +0400

    fix errors

--- app.js
+++ app.js
@@ -6,47 +6,44 @@ async function getDeepestDir() {
   let deepFile = {};
 
   async function getDeep(currentDeep, currentDirectory) {
-          fs.stat(currentDirectory, async (err, stats) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
-        
-            if (stats.isDirectory()) {
-            fs.readdir(currentDirectory, (err, data) => {          
-                if (err) {
-                  console.log('err',err);
-                }
-            
-                else {
-                  if (data.length === 0) {
-                    currentDeep++;
-                    deep = currentDeep;
-                    deepFile.deep = deep;
-                    deepFile.filePath = currentDirectory;
-                    console.log(deepFile);
-                    currentDeep--;
-                  }
-                                        
-                  data.forEach((dir) => {                                          
-                    currentDeep++;
-                    if (currentDeep >= deep && data.length > 0) {
-                      deep = currentDeep;                    
-                      deepFile.deep = deep;
-                      deepFile.filePath = currentDirectory;
-                      //console.log(deepFile);
-                    }
-                    getDeep(currentDeep,`${currentDirectory}/${dir}`);
-                      currentDeep--;
-                    })       
-                }
-              });
-            }
-        })     
+    try {
+      const stats = await fs.promises.stat(currentDirectory);
+      if (stats.isDirectory()) {
+        const data = await fs.promises.readdir(currentDirectory);
+        if (data.length === 0) {
+          currentDeep++;
+          deep = currentDeep;
+          deepFile.deep = deep;
+          deepFile.filePath = currentDirectory;
+          currentDeep--;
+        }
+
+        for (const dir of data) {
+          currentDeep++;
+          if (currentDeep >= deep && data.length > 0) {
+            deep = currentDeep;
+            deepFile.deep = deep;
+            deepFile.filePath = currentDirectory;
+          }
+          await getDeep(currentDeep, `${currentDirectory}/${dir}`);
+          currentDeep--;
+        }
+      }
+    } catch (err) {
+      console.error(err);
     }
+  }
 
-    await getDeep(currentDeep, './node_modules')
-    return deepFile;
+  await getDeep(currentDeep, './node_modules');
+  return deepFile;
 }
-
-console.log(await getDeepestDir());
+getDeepestDir()
+  .then(result => {
+    return fs.promises.writeFile(`${result.filePath}/test.txt`, 'Hello World!');
+  })
+  .then(() => {
+    console.log('File created successfully!');
+  })
+  .catch(error => {
+    console.error(error);
+  });
